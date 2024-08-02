9dbc34fd35fbfc696c8e7eff2f31b5e9d1fa6688
Gray <cheloghm@gmail.com>
2024-08-03 00:08:51 +0100

    Added portfolio page with navbar and footer

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,17 @@
-// Smooth scroll to sections
+// Smooth scroll to sections from portfolio page
 document.querySelectorAll('a.nav-link').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const href = this.getAttribute('href');
+        if (href.startsWith("index.html#")) {
+            const target = href.split("#")[1];
+            window.location.href = `index.html#${target}`;
+        } else {
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
 
         // Add active class to the current link
         document.querySelectorAll('a.nav-link').forEach(nav => nav.classList.remove('active'));
